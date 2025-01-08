4f5a3360a99a39839ba30dc1237f3f111400c512
femi-charles <femicharles99@gmail.com>
2025-01-08 16:55:25 +0100

    mess

---
+++ exo2_01.php
@@ -0,0 +1,5 @@
+<?php
+
+include ("exo2_01_2.php");
+
+echo my_included_putstr (5);
---
+++ exo2_02.php
@@ -0,0 +1,15 @@
+<?php
+
+function my_create_map(... $array){
+    foreach($array as $key => $value){
+        echo ""
+    }
+    return $array;
+
+
+}
+
+$array1 = ["pi", 3.14];
+$array2 = ["answer", 42];
+var_dump(my_create_map($array1, $array2));
+
